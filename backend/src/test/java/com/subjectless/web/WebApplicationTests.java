675eab160daedc7fbb32e844971fb7526a578846
Kim Donghyun <eastmon94@naver.com>
2021-01-13 23:36:30 +0900

    spring boot init

---
+++ backend/src/test/java/com/subjectless/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subjectless.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
